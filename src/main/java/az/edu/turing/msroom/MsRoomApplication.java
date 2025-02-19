8f3bfac3768fdb409f0d13b59a8e53fb7dc02c2f
aybeniz <aybenizkazimova15@gmail.com>
2025-02-19 17:08:17 +0400

    yoxlama commit

--- src/main/java/az/edu/turing/msroom/MsRoomApplication.java
+++ src/main/java/az/edu/turing/msroom/MsRoomApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsRoomApplication {
+    salam salam
 
     public static void main(String[] args) {
         SpringApplication.run(MsRoomApplication.class, args);
