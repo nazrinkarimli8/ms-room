4d2a1993db325d1b0cb7ea329e244099f07196d1
nazrinkarimli8 <karimlinazrin8@gmail.com>
2025-02-19 16:59:19 +0400

    commmit

---
+++ src/main/java/az/edu/turing/msroom/MsRoomApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.msroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRoomApplication.class, args);
+    }
+
+}
