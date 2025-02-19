ae65833a507b136fbd56a86cc64f9200d00b66be
nazrinkarimli8 <karimlinazrin8@gmail.com>
2025-02-19 17:12:17 +0400

    commmit

--- src/main/java/az/edu/turing/msroom/MsRoomApplication.java
+++ src/main/java/az/edu/turing/msroom/MsRoomApplication.java
@@ -2,6 +2,7 @@ package az.edu.turing.msroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class MsRoomApplication {
@@ -9,5 +10,5 @@ public class MsRoomApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsRoomApplication.class, args);
     }
-
+    //   salamm
 }
